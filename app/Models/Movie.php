13dac572a54bd31e372fa40e4b1f0da2c2ac5b08
Joshua Cadman <josh_cadman@icloud.com>
2025-10-21 22:52:11 +0100

    Refactoring formatForShow method so that movie detail page is rendered if API fails

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -27,39 +27,65 @@ class Movie extends UploadableModel
 
     public function formatForShow(): array
     {
-        return Cache::remember(
-            "movie_details_{$this->id}",
-            now()->addHour(),
-            function () {
-                $movieDetails = app(MovieLookup::class)->getMovieByName($this->title);
+        $cacheKey = "movie_details_{$this->id}";
 
-                //What is wrong with this implementation?
-                return [
-                    'id' => $this->id,
-                    'title' => $this->title,
-                    'synopsis' => $movieDetails['Plot'] ?? null,
-                    'ratings' => [
-                        'imdb' => [
-                            'score' => $movieDetails['imdbRating'] ?? null,
-                            'votes' => $movieDetails['imdbVotes'] ?? null,
-                        ],
-                        'metacritic' => [
-                            'score' => isset($movieDetails['Metascore']) && $movieDetails['Metascore'] !== 'N/A'
-                                ? $movieDetails['Metascore'] . '/100'
-                                : null,
-                        ],
-                        'rotten_tomatoes' => [
-                            'score' => collect($movieDetails['Ratings'] ?? [])->firstWhere('Source', 'Rotten Tomatoes')['Value'] ?? null,
-                        ],
-                    ],
-                    'poster' => $this->cover ?? $movieDetails['Poster'] ?? config('defaults.movie_picture'),
-                    'director' => $movieDetails['Director'] !== 'N/A' ? $movieDetails['Director'] : null,
-                    'writer' => $movieDetails['Writer'] !== 'N/A' ? $movieDetails['Writer'] : null,
-                    'actors' => isset($movieDetails['Actors']) && $movieDetails['Actors'] !== 'N/A'
-                        ? explode(', ', $movieDetails['Actors'])
-                        : [],
-                ];
-            }
-        );
+        // Try to get from cache first
+        $cached = Cache::get($cacheKey);
+        if ($cached !== null) {
+            return $cached;
+        }
+
+        // Fetch fresh data
+        $movieDetails = app(MovieLookup::class)->getMovieByName($this->title);
+
+        // If API fails, return minimal data without caching
+        if (!$movieDetails) {
+            return [
+                'id' => $this->id,
+                'title' => $this->title,
+                'synopsis' => null,
+                'ratings' => [
+                    'imdb' => ['score' => null, 'votes' => null],
+                    'metacritic' => ['score' => null],
+                    'rotten_tomatoes' => ['score' => null],
+                ],
+                'poster' => $this->poster ?? config('defaults.movie_picture'),
+                'director' => null,
+                'writer' => null,
+                'actors' => [],
+            ];
+        }
+
+        // Format successful response
+        $formatted = [
+            'id' => $this->id,
+            'title' => $this->title,
+            'synopsis' => $movieDetails['Plot'] ?? null,
+            'ratings' => [
+                'imdb' => [
+                    'score' => $movieDetails['imdbRating'] ?? null,
+                    'votes' => $movieDetails['imdbVotes'] ?? null,
+                ],
+                'metacritic' => [
+                    'score' => isset($movieDetails['Metascore']) && $movieDetails['Metascore'] !== 'N/A'
+                        ? $movieDetails['Metascore'].'/100'
+                        : null,
+                ],
+                'rotten_tomatoes' => [
+                    'score' => collect($movieDetails['Ratings'] ?? [])->firstWhere('Source', 'Rotten Tomatoes')['Value'] ?? null,
+                ],
+            ],
+            'poster' => $this->poster ?? $movieDetails['Poster'] ?? config('defaults.movie_picture'),
+            'director' => $movieDetails['Director'] !== 'N/A' ? $movieDetails['Director'] : null,
+            'writer' => $movieDetails['Writer'] !== 'N/A' ? $movieDetails['Writer'] : null,
+            'actors' => isset($movieDetails['Actors']) && $movieDetails['Actors'] !== 'N/A'
+                ? explode(', ', $movieDetails['Actors'])
+                : [],
+        ];
+
+        // Cache only successful responses
+        Cache::put($cacheKey, $formatted, now()->addHour());
+
+        return $formatted;
     }
 }
