e9c693c745b8c016fd743eed4bb78a275ba35c82
Joshua Cadman <josh_cadman@icloud.com>
2025-10-21 23:38:53 +0100

    Fixing larastan warnings for book and movie repositories

--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -4,6 +4,9 @@ namespace App\Repositories;
 
 use App\Models\Book;
 
+/**
+ * @method \Illuminate\Database\Eloquent\Collection<int, Book> getLatest(int $limit)
+ */
 class BookRepository extends BaseRepository
 {
     protected function model(): Book
--- app/Repositories/MovieRepository.php
+++ app/Repositories/MovieRepository.php
@@ -4,6 +4,9 @@ namespace App\Repositories;
 
 use App\Models\Movie;
 
+/**
+ * @method \Illuminate\Database\Eloquent\Collection<int, Movie> getLatest(int $limit)
+ */
 class MovieRepository extends BaseRepository
 {
     protected function model(): Movie
