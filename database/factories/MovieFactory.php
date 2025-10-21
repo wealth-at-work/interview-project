3c2945709d5c3416d95a85ac46ce8f80b1bd9807
Joshua Cadman <josh_cadman@icloud.com>
2025-10-21 23:13:39 +0100

    Creating factory for movies

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->name(),
+            'poster' => fake()->imageUrl(),
+            'added_by' => 1 // Manually setting user id
+        ];
+    }
+}
