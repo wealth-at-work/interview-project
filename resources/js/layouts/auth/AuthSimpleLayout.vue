6e00296a2622d526b35c40211d29209140c24928
PieroVicenti <p.vicenti@outlook.com>
2025-10-22 17:13:37 +0100

    favorite icon added books movies

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
 import { home } from '@/routes';
 import { Link } from '@inertiajs/vue3';
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -102,6 +102,20 @@ onMounted(async () => {
                         class="group"
                     >
                         <div class="bg-white border-2 border-dark-green rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+                            <div class="top-2 right-2 z-10">
+                                <svg
+                                    v-if="favoriteStore.isFavorite('book', book.id)"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                    class="w-6 h-6 text-red-500 drop-shadow"
+                                >
+                                    <path
+                                        d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
+                                    />
+                                </svg>   
+                                <span v-else>Discover more about the movie</span>                         
+                            </div>
                             <img
                                 :src="book.picture"
                                 :alt="book.title"
@@ -134,6 +148,20 @@ onMounted(async () => {
                         class="group"
                     >
                         <div class="bg-white border-2 border-dark-green rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+                            <div class="top-2 right-2 z-10">
+                                <svg
+                                    v-if="favoriteStore.isFavorite('movie', movie.id)"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                    class="w-6 h-6 text-red-500 drop-shadow"
+                                >
+                                    <path
+                                        d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
+                                    />
+                                </svg>   
+                                <span v-else>Discover more about the movie</span>                         
+                            </div>
                             <img
                                 :src="movie.picture"
                                 :alt="movie.title"
